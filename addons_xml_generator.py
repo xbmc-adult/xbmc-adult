91d11d2f7bf688384274f876084e33362532a7fd
j48antialias@gmail.com <j48antialias@gmail.com@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2011-02-23 15:02:38 +0000

    added a better addons_xml_generator.py and ran it
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@18 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -1,23 +1,27 @@
 """ addons.xml generator """
 
-import os, md5
+import os
+import md5
+
 
 class Generator:
     """
-        Generates a new addons.xml file from each addons addon.xml file.
-        Must be run from the root of the checked-out repo. Only handles
-        single depth folder structure.
+        Generates a new addons.xml file from each addons addon.xml file
+        and a new addons.xml.md5 hash file. Must be run from the root of
+        the checked-out repo. Only handles single depth folder structure.
     """
     def __init__( self ):
-        # generate file
+        # generate files
         self._generate_addons_file()
         self._generate_md5_file()
-        
+        # notify user
+        print "Finished updating addons xml and md5 files"
+
     def _generate_addons_file( self ):
         # addon list
         addons = os.listdir( "." )
         # final addons text
-        addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
+        addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n"
         # loop thru and add each addons addon.xml file
         for addon in addons:
             try:
@@ -34,7 +38,7 @@ class Generator:
                     # skip encoding format line
                     if ( line.find( "<?xml" ) >= 0 ): continue
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
+                    addon_xml += unicode( line.rstrip() + "\n", "UTF-8" )
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
             except Exception, e:
@@ -43,30 +47,26 @@ class Generator:
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
         # save file
-        self._save_addons_file( addons_xml )
-
-    def _save_addons_file( self, addons_xml ):
-        try:
-            # write the bytestring to the file
-            open( "addons.xml", "w" ).write( addons_xml.encode( "utf-8" ) )
-        except Exception, e:
-            # oops
-            print "An error occurred saving file\n%s" % ( e, )
+        self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
 
     def _generate_md5_file( self ):
         try:
-            # create a new md5 object
-            m = md5.new()
+            # create a new md5 hash
+            m = md5.new( open( "addons.xml" ).read() ).hexdigest()
+            # save file
+            self._save_file( m, file="addons.xml.md5" )
         except Exception, e:
-            print "An error occurred creating md5 object\n%s" % (e, )
-        else:
-            try:
-                # update the md5 object with the contents of addons.xml
-                m.update(open( "addons.xml").read())
-                # write md5 file
-                open( "addons.xml.md5", "w" ).write( m.hexdigest() )
-            except Exception, e:
-                print "An error occured saving md5 file\n%s" % ( e, )
+            # oops
+            print "An error occurred creating addons.xml.md5 file!\n%s" % ( e, )
+
+    def _save_file( self, data, file ):
+        try:
+            # write data to the file
+            open( file, "w" ).write( data )
+        except Exception, e:
+            # oops
+            print "An error occurred saving %s file!\n%s" % ( file, e, )
+
 
 if ( __name__ == "__main__" ):
     # start
