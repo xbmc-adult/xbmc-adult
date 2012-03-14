b289d924705bcfd72b9fc1d4e9d1944af5e94cf4
mrdougquaid <mrdougquaid@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2012-03-14 11:55:58 +0000

    Sort directory listing in addons_xml_generator
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@84 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -19,7 +19,7 @@ class Generator:
 
     def _generate_addons_file( self ):
         # addon list
-        addons = os.listdir( "." )
+        addons = sorted(os.listdir( "." ))
         # final addons text
         addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n"
         # loop thru and add each addons addon.xml file
