ecd0bc61798ecca5526e74f4845d43bcf75dab54
mrdougquaid <mrdougquaid@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2012-07-23 09:55:00 +0000

    Fixed youporn in fantasticc
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@122 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- plugin.video.fantasticc/default.py
+++ plugin.video.fantasticc/default.py
@@ -393,7 +393,7 @@ def GET_LINK(url,collections):    # Get the real video link and feed it into XBM
 			for gurl in match:
 				urlget2 = gurl
 			html = get_html(urlget2)
-			match = re.compile('.+mpg.+?</span><a href="(.+?mp4.+?)">.+?iPad').findall(html)
+			match = re.compile('</span><a href="(.+?mp4.+?)">.+?iPad').findall(html)
 			for each in match:
 				fetchurl = each.replace('&amp;','&')
 			print "fetchurl: %s" % fetchurl
