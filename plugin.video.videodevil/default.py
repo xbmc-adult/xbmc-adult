b0d7c18dcac35e790f758933b1f46afac51a31fa
mrdougquaid <mrdougquaid@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2012-09-12 15:40:36 +0000

    Cleanup default.py in videodevil   Fix indenting to 4 spaces   Remove unused imports
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@144 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- plugin.video.videodevil/default.py
+++ plugin.video.videodevil/default.py
@@ -1,16 +1,15 @@
-import xbmc, xbmcgui, xbmcaddon
-import sys, os, re
-import urllib, urllib2
+import xbmcaddon
 
 __plugin__ = 'VideoDevil'
 __author__ = 'sfaxman'
 __svn_url__ = 'http://xbmc-adult.googlecode.com/svn/trunk/plugin.video.videodevil/'
 __credits__ = 'bootsy'
-__version__ = '1.7.13'
+__version__ = '1.7.37'
 
 addon = xbmcaddon.Addon(id='plugin.video.videodevil')
 rootDir = addon.getAddonInfo('path')
-if rootDir[-1] == ';':rootDir = rootDir[0:-1]
+if rootDir[-1] == ';':
+    rootDir = rootDir[0:-1]
 
 class Main:
     def __init__(self):
@@ -19,8 +18,7 @@ class Main:
         self.run()
 
     def run(self):
-            import videodevil
-            videodevil.Main()
-            #sys.modules.clear()
+        import videodevil
+        videodevil.Main()
 
 win = Main()
