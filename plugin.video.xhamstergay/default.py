bf5683f5f525759147687a95e99b2376820fc002
mrdougquaid <mrdougquaid@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2013-05-14 11:08:49 +0000

    Cleanup xhamstergay
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@230 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- plugin.video.xhamstergay/default.py
+++ plugin.video.xhamstergay/default.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import xbmcplugin, xbmcgui, sys, urllib, urllib2, re, xbmcaddon, socket, HTMLParser
+
 socket.setdefaulttimeout(30)
 thisPlugin = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.xhamstergay')
 userAgentString = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16'
 htmlp = HTMLParser.HTMLParser()
+
+
 def index():
     addDir('Latest Videos', 'http://xhamster.com/channels/new-gays-1.html', 2)
     addDir('Search', '',3)
     xbmcplugin.endOfDirectory(thisPlugin)
+
+
 def listVideos(url):
     req = urllib2.Request(url)
     req.add_header('Cookie', 'videoFilters=%7B%22sort%22%3A%22da%22%2C%22channels%22%3A%22%3B1%22%7D; path=/; domain=xhamster.com')
@@ -34,6 +39,8 @@ def listVideos(url):
         ##addDir('PREV', 'http://xhamster.com' + url_prev,2)
     xbmcplugin.endOfDirectory(thisPlugin)
     xbmc.executebuiltin("Container.SetViewMode(500)")
+
+
 def playVideo(url):
     match=re.compile('/(\d+)/(.*)\.html',re.MULTILINE|re.DOTALL).findall(url)        
     for cod,t in match:
@@ -44,6 +51,8 @@ def playVideo(url):
     response.close()
     listitem = xbmcgui.ListItem(path=link)
     return xbmcplugin.setResolvedUrl(thisPlugin, True, listitem)
+
+
 def search():
     keyboard = xbmc.Keyboard('', 'Search')
     keyboard.doModal()
@@ -51,6 +60,8 @@ def search():
         search_string = keyboard.getText()
         url="http://xhamster.com/search.php?qcat=video&page=1&q="+urllib.quote_plus(search_string)
         listVideos(url)
+
+
 def addLink(name, url, mode=1, iconimage='', description='', isVideo=True):
     u    = sys.argv[0] + '?url=' + urllib.quote_plus(url) + '&mode=' + str(mode)
     ok   = True
@@ -62,6 +73,8 @@ def addLink(name, url, mode=1, iconimage='', description='', isVideo=True):
     liz.setProperty('IsPlayable', 'true')
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
     return ok
+
+
 def addDir(name, url, mode=1, iconimage=''):
     u   = sys.argv[0] + '?url=' + urllib.quote_plus(url) + '&mode=' + str(mode)
     ok  = True
@@ -69,6 +82,8 @@ def addDir(name, url, mode=1, iconimage=''):
     liz.setInfo(type="Video", infoLabels={ 'Title': name })
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
     return ok
+
+
 def parameters_string_to_dict(parameters):
     """Convert parameters encoded in a URL to a dict."""
     paramDict = {}
@@ -79,6 +94,8 @@ def parameters_string_to_dict(parameters):
             if (len(paramSplits)) == 2:
                 paramDict[paramSplits[0]] = paramSplits[1]
     return paramDict
+
+
 params=parameters_string_to_dict(sys.argv[2])
 mode=params.get('mode')
 url=params.get('url')
