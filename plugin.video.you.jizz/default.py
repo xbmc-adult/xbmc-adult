241b42017bbe5c718690ba7c5a09439ffda31f6e
mrdougquaid <mrdougquaid@577d15f2-bd9a-20ad-3a53-a024e8eccf28>
2013-07-31 14:54:07 +0000

    Cleanup youjizz remove trailing whitespace 2 blank lines between functions Break long lines add spaces around '=' '+' etc
    git-svn-id: https://xbmc-adult.googlecode.com/svn/trunk@250 577d15f2-bd9a-20ad-3a53-a024e8eccf28

--- plugin.video.you.jizz/default.py
+++ plugin.video.you.jizz/default.py
@@ -3,20 +3,20 @@ __author__ = "Pillager"
 __url__ = "http://code.google.com/p/xbmc-adult/"
 __scriptid__ = "plugin.video.you.jizz"
 __credits__ = "Pillager & anarchintosh"
-__version__ = "1.0.4"
+__version__ = "1.0.6"
 
 import urllib,urllib2,re
 import xbmc,xbmcplugin,xbmcgui,sys
 
 
-
-def _get_keyboard( default="", heading="", hidden=False ):
+def _get_keyboard(default="", heading="", hidden=False):
         """ shows a keyboard and returns a value """
-        keyboard = xbmc.Keyboard( default, heading, hidden )
+        keyboard = xbmc.Keyboard(default, heading, hidden)
         keyboard.doModal()
-        if ( keyboard.isConfirmed() ):
-                return unicode( keyboard.getText(), "utf-8" )
+        if (keyboard.isConfirmed()):
+                return unicode(keyboard.getText(), "utf-8")
         return default
+
 
 def CATEGORIES():
         addDir('Newest','http://www.youjizz.com/newest-clips/1.html',1,'')
@@ -25,35 +25,34 @@ def CATEGORIES():
         INDEX('http://www.youjizz.com/page/1.html')
 
 
-                       
 def INDEX(url):
         addDir('Search','http://www.youjizz.com/srch.php?q=',3,'')
         addDir('Home','',None,'')
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
-        link=response.read()
+        link = response.read()
         response.close()
-        matchname=re.compile('title1">[\n]{0,1}\s*(.+?)<').findall(link)
-        matchurl=re.compile('\/videos\/.+?(\d+).html').findall(link)
-        matchthumb=re.compile('data-original="([^"]+jpg)').findall(link)
-        matchduration=re.compile('thumbtime\'><span.*>(\d{1,}:\d{2})').findall(link)
-        for name,url,thumb,duration in zip(matchname, matchurl, matchthumb, matchduration): 
+        matchname = re.compile('title1">[\n]{0,1}\s*(.+?)<').findall(link)
+        matchurl = re.compile('\/videos\/.+?(\d+).html').findall(link)
+        matchthumb = re.compile('data-original="([^"]+jpg)').findall(link)
+        matchduration = re.compile('thumbtime\'><span.*>(\d{1,}:\d{2})').findall(link)
+        for name,url,thumb,duration in zip(matchname, matchurl, matchthumb,
+                                           matchduration):
                 url = '/videos/embed/' + url
                 addDownLink(name + ' ' + duration, url,2, thumb)
-        matchpage=re.compile('pagination[\s\S]+?<span>\d{1,}<\/span>[\s\S]+?href="(.+?html)').findall(link)
-        for nexturl in matchpage: 
+        matchpage = re.compile('pagination[\s\S]+?<span>\d{1,}<\/span>[\s\S]+?href="(.+?html)').findall(link)
+        for nexturl in matchpage:
                 addDir('Next Page','http://www.youjizz.com' + nexturl,1,'')
-
 
 
 def VIDEOLINKS(url,name):
         req = urllib2.Request('http://www.youjizz.com' + url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
-        link=response.read()
+        link = response.read()
         response.close()
-        match=re.compile('so.addVariable[(]"file",[^"]+"(http[^"]+flv[^"]+)').findall(link)
+        match = re.compile('so.addVariable[(]"file",[^"]+"(http[^"]+flv[^"]+)').findall(link)
         if not match:
                 print "Failed to find video URL"
         for url in match:
@@ -62,96 +61,101 @@ def VIDEOLINKS(url,name):
                 xbmc.Player().play(url, listitem)
 
 
-
 def SEARCHVIDEOS(url):
         searchUrl = url
-        vq = _get_keyboard( heading="Enter the query" )
+        vq = _get_keyboard(heading="Enter the query")
         # if blank or the user cancelled the keyboard, return
-        if ( not vq ): return False, 0
+        if (not vq): return False, 0
         # we need to set the title to our query
         title = urllib.quote_plus(vq)
         searchUrl += title
         print "Searching URL: " + searchUrl
         INDEX(searchUrl)
-        
 
-                
+
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
+        param = []
+        paramstring = sys.argv[2]
+        if len(paramstring) >= 2:
+                params = sys.argv[2]
+                cleanedparams = params.replace('?','')
+                if (params[len(params)-1] == '/'):
+                        params = params[0:len(params)-2]
+                pairsofparams = cleanedparams.split('&')
+                param = {}
                 for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-                                
+                        splitparams = {}
+                        splitparams = pairsofparams[i].split('=')
+                        if (len(splitparams)) == 2:
+                                param[splitparams[0]] = splitparams[1]
+
         return param
 
 
-
-
 def addDownLink(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        u = (sys.argv[0] +
+             "?url=" + urllib.quote_plus(url) +
+             "&mode=" + str(mode) +
+             "&name=" + urllib.quote_plus(name))
+        ok = True
+        liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png",
+                               thumbnailImage=iconimage)
+        liz.setInfo(type="Video", infoLabels={ "Title": name })
+        ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),
+                                         url=u, listitem=liz, isFolder=False)
         return ok
 
 
 def addDir(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        u = (sys.argv[0] +
+             "?url=" + urllib.quote_plus(url) +
+             "&mode=" + str(mode) +
+             "&name=" + urllib.quote_plus(name))
+        ok = True
+        liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png",
+                               thumbnailImage=iconimage)
+        liz.setInfo(type="Video", infoLabels={ "Title": name })
+        ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),
+                                         url=u, listitem=liz, isFolder=True)
         return ok
-        
-              
-params=get_params()
-url=None
-name=None
-mode=None
+
+
+params = get_params()
+url = None
+name = None
+mode = None
 
 try:
-        url=urllib.unquote_plus(params["url"])
+        url = urllib.unquote_plus(params["url"])
 except:
         pass
 try:
-        name=urllib.unquote_plus(params["name"])
+        name = urllib.unquote_plus(params["name"])
 except:
         pass
 try:
-        mode=int(params["mode"])
+        mode = int(params["mode"])
 except:
         pass
 
-print "Mode: "+str(mode)
-print "URL: "+str(url)
-print "Name: "+str(name)
+print "Mode: " + str(mode)
+print "URL: " + str(url)
+print "Name: " + str(name)
 
-if mode==None or url==None or len(url)<1:
+if mode == None or url == None or len(url)<1:
         print ""
         CATEGORIES()
-       
-elif mode==1:
-        print ""+url
+
+elif mode == 1:
+        print "" + url
         INDEX(url)
-        
-elif mode==2:
-        print ""+url
+
+elif mode == 2:
+        print "" + url
         VIDEOLINKS(url,name)
 
-elif mode==3:
+elif mode == 3:
         print mode
         SEARCHVIDEOS(url)
-
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
