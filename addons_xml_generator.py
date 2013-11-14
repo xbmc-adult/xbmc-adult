be1ca96965efc3261d59c69dd84805a4bd2a3680
dquaid@users.sourceforge.net <mrdougquaid@gmail.com>
2013-11-14 22:21:28 +1100

    Skip hidden directories in addons_xml_generator

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -26,7 +26,7 @@ class Generator:
         for addon in addons:
             try:
                 # skip any file or .svn folder
-                if ( not os.path.isdir( addon ) or addon == ".svn" ): continue
+                if ( not os.path.isdir( addon ) or addon.startswith('.') ): continue
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
