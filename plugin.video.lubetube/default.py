06376e9e4a0baa976f85fdb8f5b307f0e72f4e4f
mrdougquaid <mrdougquaid@gmail.com>
2018-11-20 15:29:59 +0300

    Fix url in lubetube

--- plugin.video.lubetube/default.py
+++ plugin.video.lubetube/default.py
@@ -7,9 +7,9 @@
 import urllib, urllib2, re, sys, xbmcplugin, xbmcgui
 
 def CATEGORIES():
-    addDir("Most Recent", "http://lubetube.com", 1, "")
-    link = read_url('http://lubetube.com/categories')
-    match = re.compile('<a href="(http://lubetube.com/search/adddate/cat/[^/]+/)[^>]+>([^<]+)').findall(link)
+    addDir("Most Recent", "https://lubetube.com", 1, "")
+    link = read_url('https://lubetube.com/categories')
+    match = re.compile('<a href="(https://lubetube.com/search/adddate/cat/[^/]+/)[^>]+>([^<]+)').findall(link)
     for url, title in match:
         url = url + "?page=1"
         addDir(title, url, 1, "")
@@ -19,7 +19,7 @@ def INDEX(url):
     add_next(url)
     link = read_url(url)
     match = re.compile('<a class="frame[^"]*"'
-                       ' href="(http://lubetube.com/video/[^"]+)" '
+                       ' href="(https://lubetube.com/video/[^"]+)" '
                        'title="([^"]+)">.*?<img src="([^"]+)".+?Length:([^<]+)',
                        re.DOTALL).findall(link)
     for url, name, thumbnail, length in match:
