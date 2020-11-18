95beb9850d71f9570198a7f7945af3fe26a34941
gujal <gujal@protonmail.com>
2020-11-18 14:45:53 +1300

    [EmpFlix] Matrix Compatible

--- plugin.video.empflix/addon.py
+++ plugin.video.empflix/addon.py
@@ -1,42 +1,34 @@
-from future import standard_library
-standard_library.install_aliases()
-from builtins import str
-from builtins import range
 import sys
-import urllib.request, urllib.parse, urllib.error
-import urllib.request, urllib.error, urllib.parse
-import http.cookiejar
 import re
-import xbmc
-import xbmcgui
-import xbmcplugin
-import xbmcaddon
+from six.moves import urllib_request, urllib_parse, http_cookiejar
+from kodi_six import xbmc, xbmcaddon, xbmcplugin, xbmcgui
+
 
 xbmcaddon.Addon(id='plugin.video.empflix')
-cookiejar = http.cookiejar.LWPCookieJar()
-cookie_handler = urllib.request.HTTPCookieProcessor(cookiejar)
-urllib.request.build_opener(cookie_handler)
+cookiejar = http_cookiejar.LWPCookieJar()
+cookie_handler = urllib_request.HTTPCookieProcessor(cookiejar)
+urllib_request.build_opener(cookie_handler)
 
 
 def CATEGORIES():
-    link = openURL('http://www.empflix.com/categories.php')
-    match = re.compile('/([^/]+)/\?a=1&d=" title="([^"]+)"').findall(link)
-    addDir('All', 'http://www.empflix.com/browse.php', 1, '', 1)
+    link = openURL('https://www.empflix.com/categories.php')
+    match = re.compile(r'/([^/]+)/\?a=1&d=" title="([^"]+)"').findall(link)
+    addDir('All', 'https://www.empflix.com/', 1, '', 1)
     for channame, name in match:
         addDir(name,
-               ('http://www.empflix.com/' + channame),
+               ('https://www.empflix.com/' + channame),
                2, '', 1)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 def SORTMETHOD(url):
-    if url == 'http://www.empflix.com/browse.php':
-        addDir('Being Watched', url + '?category=bw', 2, '', 1)
-        addDir('Most Recent', url + '?category=mr', 2, '', 1)
-        addDir('Most Viewed', url + '?category=mv', 2, '', 1)
-        addDir('Top Rated', url + '?category=tr', 2, '', 1)
+    if url == 'https://www.empflix.com/':
+        addDir('Featured', url + 'featured', 2, '', 1)
+        addDir('Most Recent', url + 'new', 2, '', 1)
+        addDir('Most Viewed', url + 'popular', 2, '', 1)
+        addDir('Top Rated', url + 'toprated', 2, '', 1)
     else:
-        match = re.compile('(http://www.empflix.com/channels/)'
+        match = re.compile('(https://www.empflix.com/channels/)'
                            '(.*)').findall(url)
         for start, end in match:
             addDir('Being Watched', start + 'watched-' + end, 2, '', 1)
@@ -52,21 +44,21 @@ def VIDEOLIST(url, page):
                        re.DOTALL).findall(link)
     for videourl, name, thumb, duration in match:
         addLink(name + ' (' + duration + ')',
-                'http://player.empflix.com/video/' + videourl + '?',
+                'https://player.empflix.com/video/' + videourl + '?',
                 3,
                 thumb.strip())
-    if len(match) == 24:
+    if len(match) == 60 or len(match) == 83:
         addDir('Next Page', url, 2, '', page + 1)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 def PLAYVIDEO(url):
     link = openURL(url)
-    match = re.compile('flashvars\.config\s*=\s*escape\("([^&]+)').findall(link)
+    match = re.compile(r'config\s*=\s*"([^"]+)').findall(link)
     link = openURL('https:' + match[0])
     match = re.compile('<res>.+p</[^/]+([^]]+)').findall(link)
     match.reverse()
-    xbmc.Player().play('https:' + match[0])
+    xbmc.Player().play('https:' + match[0] + '|Referer=https://www.empflix.com/')
 
 
 def get_params():
@@ -75,8 +67,8 @@ def get_params():
     if len(paramstring) >= 2:
         params = sys.argv[2]
         cleanedparams = params.replace('?', '')
-        if params[len(params)-1] == '/':
-            params = params[0:len(params)-2]
+        if params[len(params) - 1] == '/':
+            params = params[0:len(params) - 2]
         pairsofparams = cleanedparams.split('&')
         for i in range(len(pairsofparams)):
             splitparams = {}
@@ -87,22 +79,22 @@ def get_params():
 
 
 def addLink(name, url, mode, iconimage):
-    u = sys.argv[0] + '?url=' + urllib.parse.quote_plus(url) + '&mode=' + str(mode)\
-        + '&name=' + urllib.parse.quote_plus(name)
+    u = sys.argv[0] + '?url=' + urllib_parse.quote_plus(url) + '&mode=' + str(mode)\
+        + '&name=' + urllib_parse.quote_plus(name)
     ok = True
-    liz = xbmcgui.ListItem(name, iconImage='DefaultFolder.png',
-                           thumbnailImage=iconimage)
+    liz = xbmcgui.ListItem(name)
+    liz.setArt({'thumb': iconimage, 'icon': 'DefaultFolder.png'})
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u,
                                      listitem=liz, isFolder=False)
     return ok
 
 
 def addDir(name, url, mode, iconimage, page):
-    u = sys.argv[0] + '?url=' + urllib.parse.quote_plus(url) + '&mode=' + str(mode) +\
-        '&name=' + urllib.parse.quote_plus(name) + '&page=' + str(page)
+    u = sys.argv[0] + '?url=' + urllib_parse.quote_plus(url) + '&mode=' + str(mode) +\
+        '&name=' + urllib_parse.quote_plus(name) + '&page=' + str(page)
     ok = True
-    liz = xbmcgui.ListItem(name, iconImage='DefaultFolder.png',
-                           thumbnailImage=iconimage)
+    liz = xbmcgui.ListItem(name)
+    liz.setArt({'thumb': iconimage, 'icon': 'DefaultFolder.png'})
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u,
                                      listitem=liz, isFolder=True)
     return ok
@@ -110,9 +102,9 @@ def addDir(name, url, mode, iconimage, page):
 
 def openURL(url):
     xbmc.log("Opening %s" % url)
-    req = urllib.request.Request(url)
+    req = urllib_request.Request(url)
     req.add_header('Referer', 'https://www.empflix.com/')
-    response = urllib.request.urlopen(req)
+    response = urllib_request.urlopen(req)
     link = response.read().decode('utf-8')
     response.close()
     return link
@@ -125,7 +117,7 @@ def main():
     page = 1
 
     try:
-        url = urllib.parse.unquote_plus(params['url'])
+        url = urllib_parse.unquote_plus(params['url'])
     except:
         pass
     try:
