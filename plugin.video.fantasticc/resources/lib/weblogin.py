3832d360f47a6318232ff3444eafebd33bb153b6
gujal <gujal@protonmail.com>
2020-12-11 08:42:02 +1300

    [Fantasticc] Flake8 Linting

--- plugin.video.fantasticc/resources/lib/weblogin.py
+++ plugin.video.fantasticc/resources/lib/weblogin.py
@@ -26,7 +26,7 @@ import os
 from six.moves import urllib_request, urllib_parse, urllib_error, http_cookiejar
 import json
 
-### TESTING SETTINGS (will only be used when running this file independent of
+# TESTING SETTINGS (will only be used when running this file independent of
 # your addon)
 # Remember to clear these after you are finished testing,
 # so that your sensitive details are not in your source code.
@@ -34,7 +34,7 @@ import json
 # of this script.
 myusername = ''
 mypassword = ''
-#note, the cookie will be saved to the same directory as weblogin.py when testing
+# note, the cookie will be saved to the same directory as weblogin.py when testing
 
 
 def check_login(source, username):
@@ -48,13 +48,13 @@ def check_login(source, username):
 
 def doLogin(cookiepath, username, password):
 
-    #check if user has supplied only a folder path, or a full path
+    # check if user has supplied only a folder path, or a full path
     if not os.path.isfile(cookiepath):
         # if the user supplied only a folder path, append on to the end of the
         # path a filename.
         cookiepath = os.path.join(cookiepath, 'cookies.lwp')
 
-    #delete any old version of the cookie file
+    # delete any old version of the cookie file
     try:
         os.remove(cookiepath)
     except:
@@ -62,27 +62,27 @@ def doLogin(cookiepath, username, password):
 
     if username and password:
 
-        #the url you will request to.
+        # the url you will request to.
         login_url = 'https://fantasti.cc/signin.php'
 
-        #the header used to pretend you are a browser
+        # the header used to pretend you are a browser
         header_string = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
 
-        #build the form data necessary for the login
-        login_data = urllib_parse.urlencode({'user':username,
-                                       'pass':password,
-                                       'memento':1,
-                                       'x':0,
-                                       'y':0,
-                                       'do':'login',
-                                       'SSO': ''
-                                       })
+        # build the form data necessary for the login
+        login_data = urllib_parse.urlencode({'user': username,
+                                             'pass': password,
+                                             'memento': 1,
+                                             'x': 0,
+                                             'y': 0,
+                                             'do': 'login',
+                                             'SSO': ''
+                                             })
 
-        #build the request we will make
+        # build the request we will make
         req = urllib_request.Request(login_url)
         req.add_header('User-Agent', header_string)
 
-        #initiate the cookielib class
+        # initiate the cookielib class
         cj = http_cookiejar.LWPCookieJar()
 
         # Setup no redirects
@@ -90,32 +90,33 @@ def doLogin(cookiepath, username, password):
             def redirect_request(self, req, fp, code, msg, headers, newurl):
                 return None
 
-        #install cookielib into the url opener, so that cookies are handled
+        # install cookielib into the url opener, so that cookies are handled
         opener = urllib_request.build_opener(urllib_request.HTTPCookieProcessor(cj), NoRedirection())
         urllib_request.install_opener(opener)
-        #do the login and get the response
+        # do the login and get the response
 
         try:
             source = urllib_request.urlopen(req, login_data.encode()).read()
         except urllib_error.HTTPError as e:
             source = e.read()
 
-        #check the received html for a string that will tell us if the user is
+        # check the received html for a string that will tell us if the user is
         # logged in
-        #pass the username, which can be used to do this.
+        # pass the username, which can be used to do this.
         login, avatar = check_login(source, username)
 
-        #if login suceeded, save the cookiejar to disk
+        # if login suceeded, save the cookiejar to disk
         if login:
             cj.save(cookiepath)
 
-        #return whether we are logged in or not
+        # return whether we are logged in or not
         return (login, avatar)
 
     else:
         return (False, False)
 
-#code to enable running the .py independent of addon for testing
+
+# code to enable running the .py independent of addon for testing
 if __name__ == "__main__":
     if myusername == '' or mypassword == '':
         print('YOU HAVE NOT SET THE USERNAME OR PASSWORD!')
